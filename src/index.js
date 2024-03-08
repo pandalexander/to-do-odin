32b37b590995fd426b9aeb91424fcd95901514c5
Alex Seeley <alexseeley134@gmail.com>
2024-03-07 20:43:33 -0700

    Add date fns library via webpack

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import _ from "lodash";
 import "./style.css";
 import printMe from "./print.js";
 
+import { compareAsc, format } from "date-fns";
+
+format(new Date(2014, 1, 11), "yyyy-MM-dd");
+//=> '2014-02-11'
+
+const dates = [
+  new Date(1995, 6, 2),
+  new Date(1987, 1, 11),
+  new Date(1989, 6, 10),
+];
+dates.sort(compareAsc);
+//=> [
+//   Wed Feb 11 1987 00:00:00,
+//   Mon Jul 10 1989 00:00:00,
+//   Sun Jul 02 1995 00:00:00
+// ]
+
+console.log(dates[0]);
+
 function component() {
   const element = document.createElement("div");
   const btn = document.createElement("button");
