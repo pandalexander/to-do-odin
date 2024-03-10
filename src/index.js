523d289bb05d4d8aecb049cee3bf99df06e4a013
Alex Seeley <alexseeley134@gmail.com>
2024-03-09 20:43:47 -0700

    Create function that prints DOM to mirror console

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ class Project {
 
 const defaultProject = new Project("My To-Do");
 
+const secondProject = new Project("My Second To-Do");
+
 class Todo {
   constructor(title, description, dueDate, priority, project) {
     this.title = title;
@@ -22,6 +24,7 @@ class Todo {
     this.dueDate = format(dueDate, "MM-dd-yyyy");
     this.priority = priority;
     this.project = project;
+    this.projectPublicName = project.name;
     project.list.push(this);
     this.completed = false;
   }
@@ -32,6 +35,14 @@ const firstTodo = new Todo(
   "Do you Taxes, man.",
   new Date(2023, 3, 15),
   2,
+  defaultProject
+);
+
+const secondTodo = new Todo(
+  "Clean Car",
+  "Just do it",
+  new Date(2023, 2, 3),
+  0,
   defaultProject
 );
 
@@ -66,11 +77,42 @@ function deleteTodo(todoItem) {
 }
 
 function changeProject(todoItem, newProject) {
+  todoItem.project.list = todoItem.project.list.filter(
+    (object) => object.title != todoItem.title
+  );
   todoItem.project = newProject;
+  newProject.list.push(todoItem);
+  todoItem.projectPublicName = newProject.name;
 }
+
+changeProject(secondTodo, secondProject);
 
 function deleteProject(project) {
   allProjectArray = allProjectArray.filter(
     (object) => object.name != project.name
   );
+  return "HEY!";
 }
+
+// DOM MANIPULATION
+
+const body = document.getElementById("body");
+
+function printAllProjects() {
+  document.body.innerHTML = "";
+  for (const item of allProjectArray) {
+    let newItem = document.createElement("h1");
+    newItem.textContent = item.name;
+    body.appendChild(newItem);
+
+    for (let i = 0; i < item.list.length; i++) {
+      for (const property in item.list[i]) {
+        let newProp = document.createElement("p");
+        newProp.textContent = `${property}: ${item.list[i][property]}`;
+        body.appendChild(newProp);
+      }
+    }
+  }
+}
+
+printAllProjects();
